5c5b8e25f99d7fedd380754a483ba6cebe0e73d9
scissorsneedfoodtoo <scissorsneedfoodtoo@gmail.com>
2018-02-20 14:29:04 +0900

    Was able to scrape one bus schedule. Need to form the data into something more usable.

--- index.js
+++ index.js
@@ -7,13 +7,20 @@ let scrape = async () => {
   await kyungdaeAptCorner.goto('http://m.businfo.go.kr/bp/m/realTime.do?act=arrInfo&bsId=7011010100&bsNm=%B0%E6%B4%EB%BE%C6%C6%C4%C6%AE%B0%C7%B3%CA');
   // await kyungdaeAptFront.goto('http://m.businfo.go.kr/bp/m/realTime.do?act=arrInfo&bsId=7011010200&bsNm=%B0%E6%B4%EB%BE%C6%C6%C4%C6%AE%BE%D5');
 
-  const kyungdaeAptCornerBusData = [];
   // const kyungdaeAptFrontBusData = [];
 
   const kyungdaeAptCornerResult = await kyungdaeAptCorner.evaluate(() => {
     const kyungdaeAptCornerStatus = document.querySelector('#ct > div.dp > div > h3').innerText;
+    const busses = document.querySelectorAll('#ct > div.dp > ul li');
+    const kyungdaeAptCornerBusData = [];
 
-    return kyungdaeAptCornerStatus;
+    kyungdaeAptCornerBusData.push(kyungdaeAptCornerStatus);
+
+    for (let bus of busses) {
+      kyungdaeAptCornerBusData.push(bus.outerHTML);
+    }
+
+    return kyungdaeAptCornerBusData;
   })
 
   browser.close();
