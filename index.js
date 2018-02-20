0256b2d08e4a5eb682ae913d893ffaa3950731c0
scissorsneedfoodtoo <scissorsneedfoodtoo@gmail.com>
2018-02-20 21:15:52 +0900

    Refactored and basically working. Want to improve and automate more so that the code doesn't have to be changed drastically if adding more bus stops to check.

--- index.js
+++ index.js
@@ -3,64 +3,43 @@ const puppeteer = require('puppeteer');
 const kyungdaeAptCorner = 'http://m.businfo.go.kr/bp/m/realTime.do?act=arrInfo&bsId=7011010100&bsNm=%B0%E6%B4%EB%BE%C6%C6%C4%C6%AE%B0%C7%B3%CA';
 const kyungdaeAptFront = 'http://m.businfo.go.kr/bp/m/realTime.do?act=arrInfo&bsId=7011010200&bsNm=%B0%E6%B4%EB%BE%C6%C6%C4%C6%AE%BE%D5';
 
-async function scrape(url) {
+busArrivalInfo = [];
 
+async function scrape(url) {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(url);
+
+  const results = await page.evaluate(() => {
+    const busses = document.querySelectorAll('#ct > div.dp > ul li');
+
+    let busStopObj = {
+      busStopNameAndStatus: document.querySelector('#ct > div.dp > div > h3').innerText,
+      busses: []
+    };
+
+    for (let bus of busses) {
+      let currBus = {
+        routeNo: bus.childNodes[0].textContent,
+        arrState: bus.childNodes[1].textContent,
+        currPos: bus.childNodes[2].textContent,
+        urgent: bus.outerHTML.match(/class="st"/) ? true : false // check the outerHTML string for a unique class for a nearby bus and style red later
+      };
+
+      busStopObj.busses.push(currBus);
+    }
+
+    return busStopObj;
+  });
+
+  await page.close();
+  await browser.close();
+
+  return busArrivalInfo.push(results);
 }
 
-// let scrape = async () => {
-//   const browser = await puppeteer.launch();
-//   const kyungdaeAptCorner = await browser.newPage();
-//   const kyungdaeAptFront = await browser.newPage();
-//   await kyungdaeAptCorner.goto('http://m.businfo.go.kr/bp/m/realTime.do?act=arrInfo&bsId=7011010100&bsNm=%B0%E6%B4%EB%BE%C6%C6%C4%C6%AE%B0%C7%B3%CA');
-//   await kyungdaeAptFront.goto('http://m.businfo.go.kr/bp/m/realTime.do?act=arrInfo&bsId=7011010200&bsNm=%B0%E6%B4%EB%BE%C6%C6%C4%C6%AE%BE%D5');
-//   let finalResult = {};
-//
-//   finalResult['kyungdaeAptCorner'] = await kyungdaeAptCorner.evaluate(() => {
-//     const busses = document.querySelectorAll('#ct > div.dp > ul li');
-//     let kyungdaeAptCornerBusData = {
-//       busStopNameAndStatus: document.querySelector('#ct > div.dp > div > h3').innerText,
-//       busses: []
-//     };
-//
-//     for (let bus of busses) {
-//       let currBus = {
-//         routeNo: bus.childNodes[0].textContent,
-//         arrState: bus.childNodes[1].textContent,
-//         currPos: bus.childNodes[2].textContent,
-//         urgent: bus.outerHTML.match(/class="st"/) ? true : false // check the outerHTML string for a unique class for a nearby bus and style red later
-//       };
-//
-//       kyungdaeAptCornerBusData.busses.push(currBus);
-//     }
-//
-//     return kyungdaeAptCornerBusData;
-//   });
-//
-//   finalResult['kyungdaeAptFront'] = await kyungdaeAptFront.evaluate(() => {
-//     const busses = document.querySelectorAll('#ct > div.dp > ul li');
-//     let kyungdaeAptFrontBusData = {
-//       busStopNameAndStatus: document.querySelector('#ct > div.dp > div > h3').innerText,
-//       busses: []
-//     };
-//
-//     for (let bus of busses) {
-//       let currBus = {
-//         routeNo: bus.childNodes[0].textContent,
-//         arrState: bus.childNodes[1].textContent,
-//         currPos: bus.childNodes[2].textContent,
-//         urgent: bus.outerHTML.match(/class="st"/) ? true : false // check the outerHTML string for a unique class for a nearby bus and style red later
-//       };
-//
-//       kyungdaeAptFrontBusData.busses.push(currBus);
-//     }
-//
-//     return kyungdaeAptFrontBusData;
-//   });
-//
-//   browser.close();
-//   return finalResult;
-// }
-
-// scrape().then((value) => {
-//   console.log(value);
-// });
+scrape(kyungdaeAptCorner).then(() => {
+  return scrape(kyungdaeAptFront)
+}).then(() => {
+  console.log(busArrivalInfo)
+})
