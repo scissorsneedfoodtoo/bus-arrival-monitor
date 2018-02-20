d84a7000301b7e8b447c0c6041e7eb1950b8e9d6
scissorsneedfoodtoo <scissorsneedfoodtoo@gmail.com>
2018-02-20 12:15:25 +0900

    Finished basic tutorial. Now working on scraping bus data.

--- test.js
+++ test.js
@@ -1,8 +1,9 @@
 const puppeteer = require('puppeteer');
 
 async function getPic() {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({headless: false});
   const page = await browser.newPage();
+  await page.setViewport({width: 1920, height: 1080});
   await page.goto('https://google.com');
   await page.screenshot({path: 'google.png'});
 
