4eebf1b9913eb97b5e683101f603e5b391313a5a
scissorsneedfoodtoo <scissorsneedfoodtoo@gmail.com>
2018-03-03 16:23:50 +0900

    Commented out logger in app.js, and made some changes to the css to hopefully prevent the layout from breaking when the arr-state section has long strings of info. Also changed package.json a bit so that hopefully garbage collection happens muc sooner while the server is running on the RPi.

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-app.use(logger('dev'));
+// app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
