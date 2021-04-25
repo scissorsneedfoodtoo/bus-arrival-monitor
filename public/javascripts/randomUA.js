a55a6f99d72be986b0caffb6943acb67ffcad66b
Kristofer Koishigawa <scissorsneedfoodtoo@gmail.com>
2021-04-25 19:28:05 +0900

    fix: update scraper for new site (#20)
    * fix: rework scraper to work with puppeteer
    
    * fix: remove old packages, audit all packages
    
    * fix: add back in random user agent functionality
    
    * feat: update user agent strings
    
    * fix: only show 6 busses for each stop
    
    * fix: formatting

--- public/javascripts/randomUA.js
+++ public/javascripts/randomUA.js
@@ -1,15 +1,21 @@
 const userAgents = [
-  "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.71 Safari/537.36",
-  "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36",
-  "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.62 Safari/537.36",
-  "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36",
-  "Mozilla/5.0 (X11; Linux i686; rv:64.0) Gecko/20100101 Firefox/64.0",
-  "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:64.0) Gecko/20100101 Firefox/64.0",
-  "Mozilla/5.0 (X11; Linux i586; rv:63.0) Gecko/20100101 Firefox/63.0",
-  "Mozilla/5.0 (Windows NT 6.2; WOW64; rv:63.0) Gecko/20100101 Firefox/63.0",
-  "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)",
-  "Googlebot/2.1 (+http://www.googlebot.com/bot.html)",
-  "Googlebot/2.1 (+http://www.google.com/bot.html)"
+  'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.72 Safari/537.36',
+  'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.72 Safari/537.36',
+  'Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.72 Safari/537.36',
+  'Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.72 Safari/537.36',
+  'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.72 Safari/537.36',
+  'Mozilla/5.0 (Linux; Android 10) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.66 Mobile Safari/537.36',
+  'Mozilla/5.0 (Linux; Android 10; SM-A205U) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.66 Mobile Safari/537.36',
+  'Mozilla/5.0 (Linux; Android 10; LM-Q720) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.66 Mobile Safari/537.36',
+  'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:88.0) Gecko/20100101 Firefox/88.0',
+  'Mozilla/5.0 (Macintosh; Intel Mac OS X 11.2; rv:88.0) Gecko/20100101 Firefox/88.0',
+  'Mozilla/5.0 (X11; Linux i686; rv:88.0) Gecko/20100101 Firefox/88.0',
+  'Mozilla/5.0 (Linux x86_64; rv:88.0) Gecko/20100101 Firefox/88.0',
+  'Mozilla/5.0 (iPhone; CPU iPhone OS 11_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) FxiOS/33.0 Mobile/15E148 Safari/605.1.15',
+  'Mozilla/5.0 (Android 11; Mobile; rv:68.0) Gecko/68.0 Firefox/86.0',
+  'Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_3) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Safari/605.1.15',
+  'Mozilla/5.0 (iPhone; CPU iPhone OS 14_4_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 Mobile/15E148 Safari/604.1',
+  'Mozilla/5.0 (iPad; CPU OS 14_4_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 Mobile/15E148 Safari/604.1'
 ];
 
 const randomUA = () => {
--- public/javascripts/scraper.js
+++ public/javascripts/scraper.js
@@ -1,52 +1,45 @@
-const fetch = require('node-fetch');
-const jsdom = require('jsdom');
-const { JSDOM } = jsdom;
+const puppeteer = require('puppeteer');
 const randomUA = require('./randomUA.js');
 const busStopURLs = [
-  'http://m.businfo.go.kr/bp/m/realTime.do?act=arrInfo&bsId=7061005000&bsNm=%B9%FD%BF%F8%BE%D5', // in front of court
-  'http://m.businfo.go.kr/bp/m/realTime.do?act=arrInfo&bsId=7061003600&bsNm=%B1%D7%B7%A3%B5%E5%C8%A3%C5%DA%BE%D5' // in front of grand hotel
+  'https://businfo.daegu.go.kr:8095/dbms_web/map?searchText=%EB%B2%95%EC%9B%90%EC%95%9E&wincId=09089', // in front of court
+  'https://businfo.daegu.go.kr:8095/dbms_web/map?searchText=%EA%B7%B8%EB%9E%9C%EB%93%9C%ED%98%B8%ED%85%94%EC%95%9E&wincId=02016' // in front of grand hotel
 ];
 let busArrivalInfo = [];
 
 async function scrapeBusStop(url) {
-  const html = await fetch(url, {
-    headers: {
-      'User-Agent': randomUA()
-    }
-  })
-    .then(res => res.textConverted())
-    .then(body => body);
-  const dom = new JSDOM(html);
-  const window = dom.window;
-  let busStopObj = {
-    busStopNameAndStatus: window.document.querySelector('#ct > div.dp > div > h3').textContent,
-    busses: []
-  };
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.setUserAgent(randomUA());
+  await page.goto(url, { waitUntil: 'networkidle2' });
+
   try {
-    const scrapedBusses = window.document.querySelectorAll('#ct > div.dp > ul li');
-    let bussesRunning = window.document.querySelector('#ct > div.dp > ul > li > span').innerText;
-    bussesRunning = bussesRunning !== '버스운행시간이 아닙니다.' && bussesRunning !== '현 정류소에서 10정류소 이내에 있는 버스의 노선만 조회됩니다.';
+    const busStopObj = await page.evaluate(() => {
+      const scrapedBusses = Array.from(document.querySelectorAll("[id^='selectRoute'] .list-group-item")).filter((__, i) => i < 6);
 
-    if (bussesRunning) {
-      // loop through the busses NodeList and create an object for each bus
-      for (let bus of scrapedBusses) {
-        let currBus = {
-          routeNo: bus.getElementsByClassName('marquee')[0].textContent,
-          arrState: bus.getElementsByClassName('arr_state')[0].textContent,
-          currPos: bus.getElementsByClassName('cur_pos')[0].textContent,
-          urgent: bus.classList[0] === 'st' ? true : false // check the outerHTML string for a unique class for a nearby bus and style red later
-        };
+      return {
+        busStopNameAndStatus: document.querySelector('#selectBS0 > a > div.form-inline > h6').innerText,
+        busses: scrapedBusses.map(bus => {
+          const urgentClass = bus.querySelector('li:nth-child(1) > div:nth-child(3) > span').classList[0];
+    
+          return {
+            routeNo: bus.getElementsByClassName('mb-0')[0].innerText,
+            arrState: bus.getElementsByClassName('arrMin')[0].innerText,
+            currPos: bus.querySelector('div.marquee-parent > div > div').innerText,
+            urgent: urgentClass === 'text-danger' || urgentClass === 'text-danger2' ? true : false
+          }
+        })
+      };
+    });
 
-        // push the bus object to the busses array of busStopObj
-        busStopObj.busses.push(currBus);
-      } // end for bus of busses
-    } // end if bussesRunning
-  } finally {
     busArrivalInfo.push(busStopObj);
-  } // end try / finally
+  } catch(err) {
+    console.error(err);
+  } finally {
+    browser.close();
+  }
 }
 
-function compareBusStopNames(a, b) {
+function compareBusStopNames(a, __) {
   if (a.busStopNameAndStatus.includes('그랜드호텔앞')) { // show grand hotel first
     return 0;
   } else {
